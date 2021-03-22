2fc9c4b652b282401abd1af7a41d89188ca35285
marinescuana11 <marinescuana28@gmail.com>
2021-03-22 18:06:54 +0200

    Some changes about arduino code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,31 +1,13 @@
-int msensor = A1; // moisture sensor is connected with the analog pin A1 of the Arduino
+int msensor = A0; // moisture sensor is connected with the analog pin A0 of the Arduino
 int msvalue = 0; // moisture sensor value 
-int led = 13; 
-boolean flag = false; 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(msensor, INPUT);
-  pinMode(led, OUTPUT);  
+  pinMode(msensor, INPUT);  
 }
  
 void loop() {
   msvalue = analogRead(msensor);
   Serial.println(msvalue);
-  
-  if ( (msvalue >= 500  ) && ( flag == false ) )
-{
-  digitalWrite(led, HIGH); 
-  flag = true; 
-  delay(1000); 
-}
- 
-  if ( (msvalue <= 300  ) && ( flag == true ) )
-{
-  digitalWrite(led, LOW); 
-  flag = false; 
-  delay(1000); 
-}
- 
   delay(1000);      
 }
